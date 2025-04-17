f81150fc0daf99b89c2e8b1a7bd2b38e917e30ea
Danz.Code <85563725+danzprogdanz@users.noreply.github.com>
2025-04-17 17:14:19 -0300

    create login components

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+/* import { AuthService } from '../../core/services/auth.service'; */
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+}
