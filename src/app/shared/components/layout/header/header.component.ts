48b2ca28cb0349b0b9ea7762c086d536434ea7f9
Danz.Code <85563725+danzprogdanz@users.noreply.github.com>
2025-04-17 17:17:21 -0300

    create header components

---
+++ src/app/shared/components/layout/header/header.component.ts
@@ -0,0 +1,15 @@
+// shared/components/layout/header/header.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  // Add your header logic here
+}
