96aeeb47bdf05c69b2e25248d831757aded6efcb
Danz.Code <85563725+danzprogdanz@users.noreply.github.com>
2025-04-17 17:19:46 -0300

    create home components

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+/* import { AuthService } from '../../core/services/auth.service'; */
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+}
