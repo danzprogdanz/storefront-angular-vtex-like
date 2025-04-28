871513ee46001c96723919e739f28d913b6fe589
Danz.Code <85563725+danzprogdanz@users.noreply.github.com>
2025-04-28 11:28:33 -0300

    create initial auth files

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -6,8 +6,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
