211ade2b5f58058efb856226b1a71b14b0071fef
Danz.Code <85563725+danzprogdanz@users.noreply.github.com>
2025-04-28 11:16:52 -0300

    ajustments in app root configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/layout/header/header.component';
-import { HeadingComponent } from './shared/components/ui/typography/heading/heading.component';
-import { TextComponent } from './shared/components/ui/typography/text/text.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeadingComponent, TextComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'storefront-angular-vtex-like';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,20 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { HomeComponent } from './features/home/home.component';
 import { RegisterComponent } from './features/auth/register/register.component';
-import { NotFoundComponent } from './features/notFound/notFound.component';
 import { CheckoutComponent } from './features/checkout/checkout.component';
 import { CartComponent } from './features/cart/cart.component';
-import { ProductComponent } from './features/product/product.component';
+import { NotFoundComponent } from './features/notFound/not-found.component';
 
 export const routes: Routes = [
   {
     path: 'auth',
     children: [
       { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent }
-    ]
+      { path: 'register', component: RegisterComponent },
+    ],
   },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'cart', component: CartComponent },
-  { path: 'product', component: ProductComponent },
   { path: '', component: HomeComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: '**', component: NotFoundComponent },
 ];
