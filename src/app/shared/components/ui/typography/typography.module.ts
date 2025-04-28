9fe6274128e4251d27c25ca0a7571c998a9d5088
Danz.Code <85563725+danzprogdanz@users.noreply.github.com>
2025-04-28 11:25:36 -0300

    remove some files

--- src/app/shared/components/ui/typography/typography.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { HeadingComponent } from "./heading/heading.component";
-import { TextComponent } from "./text/text.component";
-
-@NgModule({
-  declarations: [HeadingComponent, TextComponent],
-  exports: [HeadingComponent, TextComponent]
-})
-export class TypographyModule {}
