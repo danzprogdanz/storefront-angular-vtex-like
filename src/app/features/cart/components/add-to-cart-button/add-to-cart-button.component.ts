75d93824832bc54b03c1ef300c5dce650ae0daf6
Danz.Code <85563725+danzprogdanz@users.noreply.github.com>
2025-04-29 10:16:51 -0300

    ajustments in add to cart button

--- src/app/features/cart/components/add-to-cart-button/add-to-cart-button.component.ts
+++ src/app/features/cart/components/add-to-cart-button/add-to-cart-button.component.ts
@@ -1,20 +1,26 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CommonModule } from '@angular/common';
+import * as CartActions from '../../state/cart.actions';
 import { Product } from '../../../../shared/types/product.types';
 
 @Component({
   selector: 'app-add-to-cart-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-to-cart-button.component.html',
   styleUrls: ['./add-to-cart-button.component.scss']
 })
 export class AddToCartButtonComponent {
+  private store = inject(Store); // Modern Angular injection
+
   @Input() product!: Product;
+  @Input() sku?: string;
   @Output() added = new EventEmitter<Product>();
 
   addToCart() {
-    // You can add cart logic here later
-    console.log('Adding to cart:', this.product);
     this.added.emit(this.product);
+    console.log('Dispatching addToCart action');
+    this.store.dispatch(CartActions.addToCart({ product: this.product }));
   }
 }
